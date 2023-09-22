0555a4dd0b19958d78637f10fe0dbe59322a9912
Mohammadreza Elahi <mohammadreza.elahi6563@gmail.com>
2023-09-22 10:43:21 +0000

    Finish caching

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -2,11 +2,12 @@ from django.contrib.auth import get_user_model
 from django import template
 from django.utils.html import format_html
 from blog.models import Post
+import logging
 
 
 register = template.Library()
 user_model = get_user_model()
-
+logger = logging.getLogger(__name__)
 
 # @register.filter
 # def author_details(author):
@@ -83,6 +84,7 @@ def author_details_tag(context):
 @register.inclusion_tag("blog/post-list.html")
 def recent_posts(post):
     posts = Post.objects.exclude(pk=post.pk)[:5]
+    logger.debug("Loaded %d recent posts for post %d", len(posts), post.pk)
     return {"title": "Recent Posts", "posts": posts}
 
 
--- blog/views.py
+++ blog/views.py
@@ -3,10 +3,14 @@ from django.utils import timezone
 from blog.models import Post
 from blog.forms import CommentForm
 import logging
+# from django.views.decorators.cache import cache_page
+# from django.views.decorators.vary import vary_on_cookie
 
 
 logger = logging.getLogger(__name__)
 
+# @cache_page(300)
+# @vary_on_cookie
 def index(request):
     posts = Post.objects.filter(published_at__lte=timezone.now())
     logger.debug("Got %d posts", len(posts))
